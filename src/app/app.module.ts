5d73780d61e7aaa9aded0999b4528017186cdf1e
Alaa Elsaka <alaa.elsaka@cognizant.com>
2021-04-29 22:47:45 -0500

    First Assigment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {DashboardComponent} from './dashboard.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,18 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html'
+}
+)
+export class DashboardComponent{
+
+status = "Loading";
+
+
+onUpdateStatus(){
+    this.status = "Finished Loading";
+}
+
+}
